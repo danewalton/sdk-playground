380d64608c137ec992e0d7df534891433cd5b000
Dane Walton <dane.walton@microsoft.com>
2020-04-29 15:40:34 -0700

    add example code

--- src/main.c
+++ src/main.c
@@ -1,7 +1,12 @@
 
+#include "stdio.h"
+#include <az_iot_core.h>
 
 int main()
 {
+  az_iot_status status;
+  az_result result = az_iot_get_status_from_uint32(200, &status);
+  printf("Status was %i\n", status);
 
   return 0;
 }
